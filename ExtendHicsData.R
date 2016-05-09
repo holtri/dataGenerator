dd57cd9a84d6c48796baba84e1e820d6c277730e
Holger <holger.trittenbach@gmail.com>
2016-05-09 17:28:16 +0200

    added option for symmetric and asymmetric subspaces

--- ExtendHicsData.R
+++ ExtendHicsData.R
@@ -1,15 +1,52 @@
 require(data.table)
+require(rgl)
 
-datasetNumber <- 7
+# parameter ---------------------------------------------------------------
+
+datasetNumber <- '7'
+
 numRelevantDim <- 150
 numNonRelevant <- 50
+
 numObjects <- 2000
-maxSubspaceSize <- 6
+
 minSubspaceSize <- 2
+maxSubspaceSize <- 6
 numOutliersPerSubspace <- 4
+# intervals <- list(c(0, 0.2), c(0.5, 0.8))
+intervals <- list(c(0, 0.2))
+symmetric <- F
+# intervals <- list(c(0, 0.1), c(0.2, 0.3), c(0.4, 0.5), c(0.5, 0.6), c(0.7, 0.8), c(0.9, 1)
+
+# functions ---------------------------------------------------------------
+
+inInterval <- function(intervals, x){
+  any(sapply(intervals, function(a) a[1] < x & a[2] > x))
+}
+
+plotSubspace <- function(data, subspace){
+  if(length(subspace) > 3){
+    stop("cannot plot subspace with more than 3 dimensions")
+  }else if(length(subspace) == 3){
+    plot3d(subset(data, select=subspace))
+  }else{
+    plot(subset(data, select=subspace))
+  }
+}
 
 randomData <- as.data.table(matrix(runif(numObjects*numRelevantDim), numObjects, numRelevantDim))
 
+## ---- generateNonRelevantDimensions
+for(i in 1:(numNonRelevant /2)){
+  x1 <- runif(numObjects)
+  x2 <- sapply(x1, function(z) z + rnorm(1, 0, 0.01))
+  randomData <- cbind(randomData, x1, x2)
+}
+names(randomData) <- paste0("var_",formatC(1:ncol(randomData), width=4, format="d", flag="0"))
+
+## ---- end
+
+## ---- generateRandomSubspaces
 counter <- 1
 subspaces <- vector(mode="list")
 
@@ -18,24 +55,23 @@ while(counter <= numRelevantDim - 2*minSubspaceSize ) {
   subspaces[[length(subspaces)+1]] <- c(counter:(counter+tmp))
   counter <- counter + tmp + 1
 }
-
 subspaces[[length(subspaces)+1]] <- c(counter:numRelevantDim)
-
-for(i in 1:(numNonRelevant /2)){
-  x1 <- runif(numObjects)
-  x2 <- sapply(x1, function(z) z + rnorm(1, 0, 0.01))
-  randomData <- cbind(randomData, x1, x2)
-}
-
-names(randomData) <- paste0("var_",formatC(1:ncol(randomData), width=4, format="d", flag="0"))
+## ---- end
 
 #create correlated subspaces
 for(s in subspaces){
   for(i in 1:numObjects){
-    j <- as.integer(ceiling(runif(1, (s[1] -1), s[length(s)])))
-    set(randomData, i=i, j=j, value=runif(1, -0.01, 0.01))
-    # set(randomData, i=i, j=j, value=runif(1, 0, 0.2))
-    # set(randomData, i=i, j=j, value=max(0,randomData[[i,j]] - runif(1, 0, 0.2)))
+    if(symmetric){
+      j <- as.integer(ceiling(runif(1, (s[1] - 1), s[length(s)])))
+      updateCol <- j
+    }
+    else{
+      j <- as.integer(ceiling(runif(1, (s[1] - 1), s[length(s)] - 1)))   
+      updateCol <- s[length(s)]
+    }
+    while(!inInterval(intervals, randomData[[i,updateCol]])){
+      set(randomData, i=i, j=updateCol, value=runif(1, 0, 1))
+    }
   }
 }
 randomData$class <- 0
@@ -51,7 +87,7 @@ for(s in subspaces){
 
 numberOutliers <- nrow(randomData[randomData$class==1])
 
-subspaces
-numberOutliers
+print(paste("number of generated outliers: ", numberOutliers))
+print(paste("number of generated subspaces: ", length(subspaces)))
 write.table(randomData, file=paste0("synth_multidim_", numNonRelevant + numRelevantDim, "_00", datasetNumber, "_labeled.csv"), sep=";")
-lapply(subspaces, write, paste0("synth_multidim_",numNonRelevant + numRelevantDim, "_00", datasetNumber, "_labeled.info"), append=TRUE, ncolumns=1000)
+invisible(lapply(subspaces, write, paste0("synth_multidim_",numNonRelevant + numRelevantDim, "_00", datasetNumber, "_labeled.info"), append=TRUE, ncolumns=1000))
